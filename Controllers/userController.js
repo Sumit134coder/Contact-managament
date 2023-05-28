b1e97dbdc2b6b335fb8d07ec0242faa4cb5779a3
Sumit134Coder <mehrasumit356@gmail.com>
2023-05-28 09:12:48 +0530

    update : password encryption

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler')
 const Users = require('../models/userSchema')
+const bcrypt = require('bcrypt');
 
 const login = asyncHandler(async(req,res) => {
     res.status(200).json({
@@ -12,10 +13,29 @@ const register = asyncHandler(async(req,res) => {
 
     const body = req.body;
 
-    const newUser = await Users.create(body)
+    const isUserExist = await Users.findOne({
+        email : body.email
+    })
+
+    console.log(isUserExist);
+
+    if(isUserExist) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+     // hashing password where password is passed along with salt rounds
+    const hashedPassword  = await bcrypt.hash(body.password , 10)
+
+    const newUser = await Users.create({
+        ...body,
+        password : hashedPassword
+        })
+
+    // const newUser = await Users.create(body)
 
 
-    res.status(200).json({
+    res.status(201).json({
         message : 'register',
         data : newUser
     })
