0a7a0dd9f2d433ac9699aa2360a3009e0688a84c
Sumit134Coder <mehrasumit356@gmail.com>
2023-05-28 09:31:27 +0530

    update : login ( authentications and token )

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,46 +1,72 @@
-const asyncHandler = require('express-async-handler')
-const Users = require('../models/userSchema')
-const bcrypt = require('bcrypt');
+const asyncHandler = require("express-async-handler");
+const Users = require("../models/userSchema");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const login = asyncHandler(async(req,res) => {
-    res.status(200).json({
-        message : 'Login'
-    })
-})
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
+  const user = await Users.findOne({
+    email,
+  });
 
-const register = asyncHandler(async(req,res) => {
+  if (!user) {
+    res.status(404);
+    throw new Error("User with this email doesn't exits");
+  }
 
-    const body = req.body;
+  // compare passwords
+  const validPassword = await bcrypt.compare(password , user.password)
+  console.log('v' , validPassword);
 
-    const isUserExist = await Users.findOne({
-        email : body.email
-    })
+  if(!validPassword){
+    res.status(401);
+    throw new Error("Incorrect password!");
+  }
 
-    console.log(isUserExist);
+  const accessToken = jwt.sign({ user }, process.env.JWT_STRING, {
+    expiresIn: "3h",
+  });
 
-    if(isUserExist) {
-        res.status(400);
-        throw new Error('User already exists');
-    }
+  res.status(200).json({
+    message: "User logged in successfully",
+    data: {
+      accessToken: accessToken,
+    },
+  });
+});
 
-     // hashing password where password is passed along with salt rounds
-    const hashedPassword  = await bcrypt.hash(body.password , 10)
+const register = asyncHandler(async (req, res) => {
+  const body = req.body;
 
-    const newUser = await Users.create({
-        ...body,
-        password : hashedPassword
-        })
+  const isUserExist = await Users.findOne({
+    email: body.email,
+  });
 
-    // const newUser = await Users.create(body)
+  console.log(isUserExist);
 
+  if (isUserExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
-    res.status(201).json({
-        message : 'register',
-        data : newUser
-    })
-})
+  // hashing password where password is passed along with salt rounds
+  const hashedPassword = await bcrypt.hash(body.password, 10);
+
+  const newUser = await Users.create({
+    ...body,
+    password: hashedPassword,
+  });
+
+  // const newUser = await Users.create(body)
+
+  res.status(201).json({
+    message: "register",
+    data: newUser,
+  });
+});
 
 module.exports = {
-    login , register
-}
+  login,
+  register,
+};
